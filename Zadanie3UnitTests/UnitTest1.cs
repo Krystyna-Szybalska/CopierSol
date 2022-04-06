3d43670e354b7c89d34aaaefc7cf71dab38531bd
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2022-04-06 19:56:33 +0200

    deleted wrong projects

---
+++ Zadanie3UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Zadanie3UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
